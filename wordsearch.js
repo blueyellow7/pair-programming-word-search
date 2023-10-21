b2a4a8fdde12fa28e493977416cc0b252b2d56e6
Neeharika Baral <neehabaral72@gmail.com>
2023-10-21 15:52:31 -0400

    inital commit Co-authored-by: Mako <M-ARTHELL@users.noreply.github.com> Co-authored-by: Junkpoe <Junkpoe@users.noreply.github.com>

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    const transposeItFirst = transpose(letters);
+    let verticalJoin = transposeItFirst.map(ls => ls.join(''));
+    
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
 
-module.exports = wordSearch
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+
+    return false;
+};
+
+const transpose = function(letters) {
+    let array = [];
+        for (let i = 0; i < letters[0].length; i++) {
+          array.push([]);
+        }
+        for (let row = 0; row < letters.length; row++) {
+          for (let col = 0; col < letters[row].length; col++) {
+            array[col][row] = letters[row][col];
+          }
+        }
+        return array;
+      };
+
+module.exports = wordSearch;
+
+
+// Pair programmed by: 
+// M-ARTHELL
+// Junkpoe
+// Neeha
